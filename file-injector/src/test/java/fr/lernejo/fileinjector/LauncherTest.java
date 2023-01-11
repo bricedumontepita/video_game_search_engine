c6654cd7881e1bc972a43aef34bfe18cb1a7c2d7
Brice DUMONT <brice.dumont@epita.fr>
2023-01-11 22:24:43 +0100

    ajout library, correction test launcher 20->1

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -11,15 +11,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class LauncherTest {
 
     @Test
-    void main_terminates_before_20_sec() {
+    void main_terminates_before_1_sec() {
         assertTimeoutPreemptively(
-            Duration.ofSeconds(20L),
-            () -> Launcher.main(new String[]{"src/test/resources/games.json"}));
+            Duration.ofSeconds(1L),
+            () -> Launcher.main(new String[]{}));
     }
 
     @Test
     void testSendingMessageWithNoFile () {
         Assertions.assertThatExceptionOfType(Exception.class)
             .isThrownBy(() -> Launcher.main(new String[]{}));
+    }
+
+    @Test
+    void testSendingMessage () {
+        try {
+            Launcher.main(new String[]{"src/test/resources/games.json"});
+        } catch (Exception e) {
+
+        }
     }
 }
